1265f692c046357aa3ec095ecffb750cb848dab7
Ninadiantea <Ninadianthea@gmail.com>
2025-08-25 19:09:13 +0700

    Update user-add-trojan.sh

--- user-add-trojan.sh
+++ user-add-trojan.sh
@@ -5,5 +5,6 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 domain=$(cat /etc/diana/domain)
 expiry=$(date -d "+$exp days" +%Y-%m-%d)
 echo "$name:$uuid:$expiry" >> /etc/diana/users/trojan.txt
-echo "Trojan created: $name | Pass: $uuid | Exp: $expiry"
-echo "Link: trojan://$uuid@$domain:443?security=tls&type=ws&path=/trojan#$name"
+echo "✅ Trojan user: $name | Pass: $uuid | Exp: $expiry"
+echo "Link:"
+echo "trojan://$uuid@$domain:443?security=tls&type=ws&path=/trojan#$name"
