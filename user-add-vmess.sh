350ce18426e4e9ba31afb59eba18ed5d35d41f71
Ninadiantea <Ninadianthea@gmail.com>
2025-08-25 19:12:55 +0700

    Update user-add-vmess.sh

--- user-add-vmess.sh
+++ user-add-vmess.sh
@@ -5,5 +5,6 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 domain=$(cat /etc/diana/domain)
 expiry=$(date -d "+$exp days" +%Y-%m-%d)
 echo "$name:$uuid:$expiry" >> /etc/diana/users/vmess.txt
-echo "VMess created: $name | UUID: $uuid | Exp: $expiry"
-echo "Link: vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"$name\",\"add\":\"$domain\",\"port\":\"443\",\"id\":\"$uuid\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$domain\",\"path\":\"/vmess\",\"tls\":\"tls\"}" | base64 -w 0)"
+echo "✅ VMess user: $name | UUID: $uuid | Exp: $expiry"
+echo "Link:"
+echo "vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"$name\",\"add\":\"$domain\",\"port\":\"443\",\"id\":\"$uuid\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$domain\",\"path\":\"/vmess\",\"tls\":\"tls\"}" | base64 -w 0)"
