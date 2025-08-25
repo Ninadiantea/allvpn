33816d8d1d85a411f2a4e63e0ff17fba4d63bd88
Ninadiantea <Ninadianthea@gmail.com>
2025-08-25 18:18:02 +0700

    Create user-list.sh

---
+++ user-list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "=== SSH ==="
+awk -F: '$3>=1000 && $3<60000 {print $1}' /etc/passwd
+echo ""
+echo "=== VMess ==="
+cat /etc/diana/users/vmess.txt
+echo ""
+echo "=== VLESS ==="
+cat /etc/diana/users/vless.txt
+echo ""
+echo "=== Trojan ==="
+cat /etc/diana/users/trojan.txt
+echo ""
+echo "=== Shadow ==="
+cat /etc/diana/users/shadow.txt
