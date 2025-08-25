fb0a257b3a3d453e7699bc205a99c8b2b34487ff
Ninadiantea <Ninadianthea@gmail.com>
2025-08-25 19:08:00 +0700

    Update user-add-ssh.sh

--- user-add-ssh.sh
+++ user-add-ssh.sh
@@ -4,4 +4,4 @@ read -p "Password SSH: " pass
 read -p "Masa aktif (hari): " exp
 useradd -e $(date -d "+$exp days" +%Y-%m-%d) -s /bin/false "$user"
 echo "$user:$pass" | chpasswd
-echo "SSH User: $user | Pass: $pass | Exp: $(date -d "+$exp days")"
+echo "✅ SSH user: $user | Pass: $pass | Exp: $(date -d "+$exp days")"
